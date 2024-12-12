a9a0909b298cb8ad509e84e5bcea95f108588eb4
SithuLin12 <hack09715@gmail.com>
2024-12-12 19:22:02 +0630

    live chat added

--- src/app/[lng]/(MainLayout)/layout.js
+++ src/app/[lng]/(MainLayout)/layout.js
@@ -6,24 +6,24 @@ import { useEffect } from "react";
 import Cookies from "js-cookie";
 
 export default function RootLayout({ children, params: { lng } }) {
-  // useEffect(() => {
-  //   // Check for the authentication cookie (e.g., "uat")
-  //   const auth = Cookies.get("uat");
+  useEffect(() => {
+    // Check for the authentication cookie (e.g., "uat")
+    const auth = Cookies.get("uat");
 
-  //   if (auth) {
-  //     // If authenticated, load the Tawk.to script
-  //     const script = document.createElement("script");
-  //     script.async = true;
-  //     script.src = "https://embed.tawk.to/674961372480f5b4f5a58c13/1idra7h4r";
-  //     script.charset = "UTF-8";
-  //     script.setAttribute("crossorigin", "*");
-  //     document.body.appendChild(script);
+    if (auth) {
+      // If authenticated, load the Tawk.to script
+      const script = document.createElement("script");
+      script.async = true;
+      script.src = "https://embed.tawk.to/674961372480f5b4f5a58c13/1idra7h4r";
+      script.charset = "UTF-8";
+      script.setAttribute("crossorigin", "*");
+      document.body.appendChild(script);
 
-  //     return () => {
-  //       document.body.removeChild(script); // Cleanup on unmount
-  //     };
-  //   }
-  // }, []); // Runs once on mount
+      return () => {
+        document.body.removeChild(script); // Cleanup on unmount
+      };
+    }
+  }, []); // Runs once on mount
 
   return (
     <>
